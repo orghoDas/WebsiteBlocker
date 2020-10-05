21ef573552ed4df7ee36c30bce047763eb3b1dce
Orgho McGuire <mcguireon@gmail.com>
2020-10-05 22:25:22 +0600

    first commit

---
+++ blocker.py
@@ -0,0 +1,11 @@
+import time
+
+hosts_path = r'E:\Code\Python\Python Projects\WebsiteBlocker'
+
+redirect = '127.0.0.1'
+
+website_list = ['www.facebook.com', 'facebook.com']
+
+while True:
+    print(1)
+    time.sleep(30)
