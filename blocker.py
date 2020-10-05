88caffe73f85e55156fbb240f024fd56fe4f7b7b
Orgho McGuire <mcguireon@gmail.com>
2020-10-05 22:59:38 +0600

    first commit

--- blocker.py
+++ blocker.py
@@ -1,5 +1,8 @@
 import time
+from datetime import datetime as dt
 
+
+host_file = 'hosts'
 hosts_path = r'E:\Code\Python\Python Projects\WebsiteBlocker'
 
 redirect = '127.0.0.1'
@@ -7,5 +10,20 @@ redirect = '127.0.0.1'
 website_list = ['www.facebook.com', 'facebook.com']
 
 while True:
-    print(1)
+    if dt(dt.now().year, dt.now().month, dt.now().day,8) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day,16):
+        print('Working Hours...')
+
+        with open(host_file, 'r+') as file:
+            content = file.read()
+
+            for website in website_list:
+                if website in content:
+                    pass
+
+                else:
+                    file.write(redirect + ' ' + website + '\n')
+    
+    else:
+        print('Non-Working Hours...')
+    
     time.sleep(30)
